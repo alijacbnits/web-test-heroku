c665af3b0e64c415b49ef74e2526bcfe7468e07a
Your Name <you@example.com>
2022-09-13 17:59:30 +0530

    deployment app to heroku server

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"",
+    pathMatch:'full',
+    redirectTo: "post/list"
+  },
+  {
+    path:"post/create",
+    loadChildren: () => import('./pages/create/create.module').then((m)=>m.CreateModule)
+  },
+  {
+    path:"post/list",
+    pathMatch:'full',
+    loadChildren: () => import('./pages/list/list.module').then((m)=>m.ListModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
